8a1483b4869daf5f567e1f4b77b0ab360f6e699b
mobilelegendsbkrjd-oss <mobilelegendsbkrjd@gmail.com>
2026-03-10 09:55:45 -0600

    Update ExtractorNovelas360.kt

--- Novelas360/src/main/kotlin/com/novelas360/ExtractorNovelas360.kt
+++ Novelas360/src/main/kotlin/com/novelas360/ExtractorNovelas360.kt
@@ -1,10 +1,10 @@
 package com.novelas360
 
 import com.lagradost.cloudstream3.app
-import com.lagradost.cloudstream3.utils.*
-import com.lagradost.cloudstream3.ExtractorApi
-import com.lagradost.cloudstream3.ExtractorLink
-import com.lagradost.cloudstream3.util.Qualities
+import com.lagradost.cloudstream3.utils.ExtractorApi
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.newExtractorLink
+import com.lagradost.cloudstream3.utils.Qualities
 
 class ExtractorNovelas360 : ExtractorApi() {
 
@@ -42,8 +42,7 @@ class ExtractorNovelas360 : ExtractorApi() {
             "Origin" to mainUrl,
             "Referer" to url,
             "X-Requested-With" to "XMLHttpRequest",
-            "User-Agent" to "Mozilla/5.0",
-            "Accept" to "application/json, text/javascript, */*; q=0.01"
+            "User-Agent" to "Mozilla/5.0"
         )
 
         val data = mapOf(
@@ -70,14 +69,9 @@ class ExtractorNovelas360 : ExtractorApi() {
                 name = "Servidor Cyou",
                 url = file
             ) {
-                this.referer = url
-                this.quality = Qualities.Unknown.value
-                this.isM3u8 = file.contains(".m3u8")
-                this.headers = mapOf(
-                    "Referer" to url,
-                    "Origin" to mainUrl,
-                    "User-Agent" to "Mozilla/5.0"
-                )
+                referer = url
+                quality = Qualities.Unknown.value
+                isM3u8 = file.contains(".m3u8")
             }
         )
     }
