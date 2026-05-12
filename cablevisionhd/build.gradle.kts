a5e2d0663f0a2200585214e28c6938a93d379cb4
mobilelegendsbkrjd-oss <mobilelegendsbkrjd@gmail.com>
2026-05-11 18:17:59 -0600

    Update build.gradle.kts

--- cablevisionhd/build.gradle.kts
+++ cablevisionhd/build.gradle.kts
@@ -1,27 +1,18 @@
-version = 5
 version = 12
 
 cloudstream {
-    // All of these properties are optional, you can safely remove them
 
-    description = "(Mexican) Anime Extension"
-    language    = "mx"
+    description = "CablevisionHD Live TV"
+    language = "es"
     authors = listOf("Phisher98")
 
-    /**
-    * Status int as the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta only
-    * */
-    status = 1 // will be 3 if unspecified
+    status = 1
 
-    // List of video source types. Users are able to filter for extensions in a given category.
-    // You can find a list of available types here:
-    // https://recloudstream.github.io/cloudstream/html/app/com.lagradost.cloudstream3/-tv-type/index.html
-    tvTypes = listOf("Movie","Anime","AnimeMovie")
-    iconUrl = "https://raw.githubusercontent.com/phisher98/TVVVV/refs/heads/main/Icons/animeav1.png"
+    tvTypes = listOf(
+        "Live"
+    )
+
+    iconUrl = "https://www.google.com/s2/favicons?sz=64&domain=cablevisionhd.com"
 
     isCrossPlatform = false
 }
