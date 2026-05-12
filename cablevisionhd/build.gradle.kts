bcffa60ded2e7f08592cca6b00df4e7077122753
mobilelegendsbkrjd-oss <mobilelegendsbkrjd@gmail.com>
2026-05-11 18:14:43 -0600

    Update build.gradle.kts

--- cablevisionhd/build.gradle.kts
+++ cablevisionhd/build.gradle.kts
@@ -1,10 +1,5 @@
-// use an integer for version numbers
-<<<<<<< HEAD:cablevisionhd/build.gradle.kts
 version = 5
-=======
 version = 12
->>>>>>> upstream/master:Coflix/build.gradle.kts
-
 
 cloudstream {
     // All of these properties are optional, you can safely remove them
