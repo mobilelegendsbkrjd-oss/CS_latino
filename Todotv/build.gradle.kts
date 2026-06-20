3477fea65292f89e8530aca5d309be409da7c993
mobilelegendsbkrjd-oss <mobilelegendsbkrjd@gmail.com>
2026-06-20 16:49:20 -0600

    up

--- Todotv/build.gradle.kts
+++ Todotv/build.gradle.kts
@@ -2,8 +2,8 @@ version = 1
 
 cloudstream {
     description = "Televisión gratuita en HD de Argentina y Latinoamérica"
-    language = "es"
-    authors = listOf("TuNombre")
+    language = "mx"
+    authors = listOf("bkrjd")
     status = 1
     tvTypes = listOf("Live")
     iconUrl = "https://www.telegratishd.com/favicon.ico"
