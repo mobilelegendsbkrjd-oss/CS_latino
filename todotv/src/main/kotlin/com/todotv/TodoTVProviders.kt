cfe5da5da2e47ea15ae743a37fe28217cfdeeb65
mobilelegendsbkrjd-oss <mobilelegendsbkrjd@gmail.com>
2026-06-20 17:23:45 -0600

    Update URLs for TodoProviders in TodoTVProviders.kt

--- todotv/src/main/kotlin/com/todotv/TodoTVProviders.kt
+++ todotv/src/main/kotlin/com/todotv/TodoTVProviders.kt
@@ -11,9 +11,9 @@ object TodoTVProviders {
         TodoProvider("TvporinternetHD", "https://www.tvporinternet2.com", true),
         TodoProvider("Tv Libre Futbol", "https://www.librefutbol2.com", true),
         TodoProvider("CableVisionHD", "https://www.cablevisionhd.com", true),
-        TodoProvider("Teveplus", "https://www.tvplusgratis2.com", true),
-        TodoProvider("Telegratis", "https://www.telegratishd.com", true),
-        TodoProvider("VerCableHD", "https://www.vertvcable.com", false),
-        TodoProvider("SinTelevisor", "https://www.thesintelevisor.com", false)
+        TodoProvider("Teveplus", "https://www.tvplusgratis2.com/", true),
+        TodoProvider("Telegratis", "https://www.telegratishd.com/", true),
+        TodoProvider("VerCableHD", "https://www.vertvcable.com/", false),
+        TodoProvider("SinTelevisor", "https://www.thesintelevisor.com/", false)
     )
 }
